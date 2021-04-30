5e1de8311f686a406904ec0c5cf57df17900db02
ShebangDog <e1n18088@oit.ac.jp>
2021-04-30 17:40:59 +0900

    memberコンポーネントを使って入退室情報を表示する

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,29 @@ import Head from 'next/head'
 import styles from "../styles/Member.module.css"
 import Member from "../components/member";
 import {useEffect, useState} from "react";
+import DefaultMemberApiClient from "./api/default_member_api_client";
+import MemberApiClient from "./api/member_api_client";
 
 const home = function Home() {
-    const url = "http://localhost:8080/members"
     const [data, setData] = useState([])
+    const apiClient = new MemberApiClient(DefaultMemberApiClient)
 
     useEffect(async () => {
         try {
-            const result = await fetch(url)
-            const data = await result.json()
+            const members = await apiClient.fetchMembers()
+                .then(result => result.json())
+
+            const data = await Promise.all(members.map(async member => {
+                const event = await apiClient.fetchAccessByStudentId(member.student_id)
+                    .then(result => result.json())
+                    .then(access => access == null ? "register" : access.event)
+
+                return {
+                    "student_id": member.student_id,
+                    "name": member.name,
+                    "event": event
+                }
+            }))
 
             setData(data)
         } catch (error) {
@@ -29,7 +43,7 @@ const home = function Home() {
                 <article>
                     <h1>布村研</h1>
                     <div className={styles.memberContainer}>
-                        {data.map(member => <Member member={member} key={member.student_id}/>)}
+                        {data.map(elem => <Member data={elem} key={elem.student_id}/>)}
                     </div>
                 </article>
             </main>
