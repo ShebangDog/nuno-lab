e2d3895cf1f3a11e23373ad691c401c52c08a444
ShebangDog <e1n18088@oit.ac.jp>
2021-04-30 17:40:32 +0900

    個人の入退室情報を表すMemberコンポーネントを実装する

--- components/member.js
+++ components/member.js
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "../styles/Member.module.css";
 
 export default function Member(props) {
-    let member = props.member
+    const data = props.data
+    const isEntry = (data.event === "entry")
+    const style = {
+        backgroundColor: isEntry ? "green" : "red"
+    }
 
     return (
-        <section className={styles.member}>
+        <section className={styles.member} style={style}>
             <img src="./dog.png" alt={"member icon"} className={styles.memberIcon}/>
-            <h2>{member.name}</h2>
+            <h2>{data.name}</h2>
         </section>
     )
 }
